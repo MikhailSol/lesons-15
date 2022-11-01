faaa8f377ca0cc4fa2c8be7083604819f40fb0a6
MikhailSol <115576625+MikhailSol@users.noreply.github.com>
2022-11-01 21:45:27 +0300

    Add files via upload

--- SOLOVEV_104_lesson_17.py
+++ SOLOVEV_104_lesson_17.py
@@ -24,76 +24,76 @@
 #
 # car_object = Car()
 
-### Домашнее задание № 1 ###
-# class Example:
-#     num_1 = 10
-#     num_2 = 40
-#
-#     def __init__(self, ch_1, ch_2):
-#         self.ch_1 = ch_1
-#         self.ch_2 = ch_2
-#
-#     def fun_1(self):
-#         return self.ch_1
-#
-#     def fun_2(self):
-#         return Example.num_1 + Example.num_2
-#
-#     def fun_3(self):
-#         return self.ch_1 ** self.ch_2
-#
-#
-# a = Example(20, 5)
-#
-# print(a.fun_1())
-# print(a.fun_2())
-# print(a.fun_3())
+## Домашнее задание № 1 ###
+class Example:
+    num_1 = 10
+    num_2 = 40
 
-### Домашнее задание № 2 ###
-# class Calculator:
-#
-# # def __init__(self):
-# #     self.a = int(input("Введите число: "))
-# #     self.w = input("Введите знак(+,-,*,/): ")
-# #     self.b = int(input("Введите число: "))
-# #     if self.w == '+':
-# #         print(qwe.sloj())
-# #     elif self.w == '-':
-# #         print(qwe.otnim())
-# #     elif self.w == '*':
-# #         print(qwe.umnoj())
-# #     elif self.w == '/':
-# #         print(qwe.delen())
-#
-#         def sloj(self):
-#             return a + b
-#
-#         def otnim(self):
-#             return a - b
-#
-#         def umnoj(self):
-#             return a * b
-#
-#         def delen(self):
-#             return a / b
-#
-# qwe = Calculator()
-#
-# # Не могу сообразить как сделать. Хоть и понимаю, что не правильное такое решение.
-#
-# a = int(input('Введите число: '))
-# w = input('Введите знак(+,-,*,/): ')
-# b = int(input('Введите число: '))
-# if w == '+':
-#     print(qwe.sloj())
-# elif w == '-':
-#     print(qwe.otnim())
-# elif w == '*':
-#     print(qwe.umnoj())
-# elif w == '/':
-#     print(qwe.delen())
-# else:
-#     print("Не корректный ввод данных!")
+    def __init__(self, ch_1, ch_2):
+        self.ch_1 = ch_1
+        self.ch_2 = ch_2
+
+    def fun_1(self):
+        return self.ch_1
+
+    def fun_2(self):
+        return Example.num_1 + Example.num_2
+
+    def fun_3(self):
+        return self.ch_1 ** self.ch_2
+
+
+a = Example(20, 5)
+
+print(a.fun_1())
+print(a.fun_2())
+print(a.fun_3())
+
+## Домашнее задание № 2 ###
+class Calculator:
+
+# def __init__(self):
+#     self.a = int(input("Введите число: "))
+#     self.w = input("Введите знак(+,-,*,/): ")
+#     self.b = int(input("Введите число: "))
+#     if self.w == '+':
+#         print(qwe.sloj())
+#     elif self.w == '-':
+#         print(qwe.otnim())
+#     elif self.w == '*':
+#         print(qwe.umnoj())
+#     elif self.w == '/':
+#         print(qwe.delen())
+
+        def sloj(self):
+            return a + b
+
+        def otnim(self):
+            return a - b
+
+        def umnoj(self):
+            return a * b
+
+        def delen(self):
+            return a / b
+
+qwe = Calculator()
+
+# Не могу сообразить как сделать. Хоть и понимаю, что не правильное такое решение.
+
+a = int(input('Введите число: '))
+w = input('Введите знак(+,-,*,/): ')
+b = int(input('Введите число: '))
+if w == '+':
+    print(qwe.sloj())
+elif w == '-':
+    print(qwe.otnim())
+elif w == '*':
+    print(qwe.umnoj())
+elif w == '/':
+    print(qwe.delen())
+else:
+    print("Не корректный ввод данных!")
 
 ### Домашнее задание № 3 ###
 
@@ -102,7 +102,7 @@
 #     def priem(self):
 #         if
 
-text = input('Ведите число и слово: ')
+text = input('Ведите число и слово: ')         # нет понятия по решению. Надо еще раз поднять старые темы.
 a = 1
 for i in text:
     if i.isdigit():
